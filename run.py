5114e0cde0988013e75ef0c58539d98797768bdb
vliedel <bart@crownstone.rocks>
2020-06-16 16:47:07 +0200

    Updated to new lib.

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 import signal
 
-from BluenetLib import Bluenet, BluenetEventBus, Topics
-from BluenetLib.lib.topics.DevTopics import DevTopics
+from crownstone_uart import CrownstoneUart, UartEventBus, UartTopics
+from crownstone_uart.topics.DevTopics import DevTopics
 
 from BluenetWebSocket import WebSocketServer
 from BluenetWebSocket.lib.connector.BluenetConnector import BluenetConnector
@@ -9,19 +11,19 @@ from BluenetWebSocket.lib.connector.BluenetConnector import BluenetConnector
 from parser.WSParser import WSParser
 
 # Create new bluenet instance
-bluenet = Bluenet(catchSIGINT=False)
+bluenet = CrownstoneUart()
 
 # Start up the USB bridge
-bluenet.initializeUSB("/dev/tty.usbmodemFA1331", baudrate=230400)
+bluenet.initialize_usb_sync("/dev/ttyACM0")
 
 # start the websocket server
 server = WebSocketServer(9000)
 
-# connect the websocket server to bluenet
-server.connectToBluenet(bluenet, BluenetEventBus, Topics)
+# connect the websocket server to bluenet lib
+server.connectToBluenet(bluenet, UartEventBus, UartTopics)
 
 connector = BluenetConnector()
-connector.connect(BluenetEventBus, DevTopics)
+connector.connect(UartEventBus, DevTopics)
 
 # add our custom parser
 customParser = WSParser()
